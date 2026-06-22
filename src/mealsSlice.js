129d202676b37add2bac87b9fbeb08a4bb4d0165
Sneyder-dev <sney11gomez@gmail.com>
2026-06-22 18:26:58 -0400

    More changes

--- src/mealsSlice.js
+++ src/mealsSlice.js
@@ -11,10 +11,10 @@ export const mealsSlice = createSlice({
   ],
   reducers: {
     toggleMealSelection: (state, action) => {
+        state[action.payload].selected = !state[action.payload.selected];
     },
   },
 });
 
 export const { toggleMealSelection } = mealsSlice.actions;
-
 export default mealsSlice.reducer;
